83a44f79bcb8e1683f5b6c2e7bdfdb08469cbd82
Beale1985 <alexisbello0310@hotmail.com>
2022-03-24 19:14:22 -0300

    Agregue el carrousel y el header

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require('express');
+const app = express();
+
+app.listen (3030, () =>
+console.log ("Hola")
+);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"))
+})
+
+app.get("/Product", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/product.html"))
+})
+
+app.get("/views/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+})
+
+app.get("/*", (req, res) => {
+    res.status(404).send('404 not found. <br> ¡Houston, poseemos problemas!');
+});
