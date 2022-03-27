dbd34781753c22c2817231dd838d45cbd482eea5
Beale1985 <alexisbello0310@hotmail.com>
2022-03-27 19:57:32 -0300

    armé el registro y mejoré el home

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ console.log ("Hola")
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/home.html"))
+    res.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
-app.get("/Product", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/product.html"))
+app.get("/views/product", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/product.html"))
 })
 
 app.get("/views/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/login.html"))
+    res.sendFile(path.join(__dirname, "./views/login.html"))
 })
 
-app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/register.html"))
+app.get("/views/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"))
 })
 
 app.get("/*", (req, res) => {
